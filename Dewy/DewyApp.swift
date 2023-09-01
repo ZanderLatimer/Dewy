804bb27fa4e89fa503dae87b30e4606610740148
Alexander Latimer <zanderlatimer@gmail.com>
2023-09-01 13:59:26 -0700

    Initial Commit

---
+++ Dewy/DewyApp.swift
@@ -0,0 +1,17 @@
+//
+//  DewyApp.swift
+//  Dewy
+//
+//  Created by Zander Latimer on 9/1/23.
+//
+
+import SwiftUI
+
+@main
+struct DewyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
