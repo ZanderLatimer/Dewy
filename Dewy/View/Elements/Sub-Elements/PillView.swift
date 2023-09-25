6fbb5e744435ba5e846b548a8b9e181d09f6697c
Alexander Latimer <zanderlatimer@gmail.com>
2023-09-25 12:21:09 -0700

    Added a PillView for displaying colour coded information.

---
+++ Dewy/View/Elements/Sub-Elements/PillView.swift
@@ -0,0 +1,43 @@
+//
+//  PillView.swift
+//  Dewy
+//
+//  Created by Zander Latimer on 9/13/23.
+//
+
+import SwiftUI
+
+struct PillView: View {
+    
+    enum PillSize {
+        case small
+        case large
+    }
+    
+    private let value: String
+    private let colour: Color
+    // TODO: Apply this
+    private let size: PillSize
+    
+    var body: some View {
+        Text(value)
+            .font(.footnote)
+            .fontWeight(.semibold)
+            .foregroundStyle(.black)
+            .padding(6)
+            .background(colour)
+            .clipShape(RoundedRectangle(cornerRadius: 100))
+    }
+    
+    init(value: String, colour: Color, size: PillSize = .small) {
+        self.value = value
+        self.colour = colour
+        self.size = size
+    }
+}
+
+struct PillView_Previews: PreviewProvider {
+    static var previews: some View {
+        PillView(value: "UVI 10", colour: .indigo)
+    }
+}
