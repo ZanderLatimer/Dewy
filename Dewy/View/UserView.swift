27c2f555764a7ed4efb458852839c45ea4ae31af
Alexander Latimer <zanderlatimer@gmail.com>
2023-09-25 12:27:57 -0700

    Implemented first cut of settings and user information in the User tab view.

--- Dewy/View/UserView.swift
+++ Dewy/View/UserView.swift
@@ -9,17 +9,52 @@ import SwiftUI
 
 struct UserView: View {
     
+    @AppStorage(UserDefaults.Keys.UserView.userName) var userName: String = ""
+    @AppStorage(UserDefaults.Keys.UserView.skinType) var userSkinType: SkinType = .normal
+    @AppStorage(UserDefaults.Keys.UserView.temperatureUnit) var preferredTemperatureUnit: TemperatureUnit = .celsius
+    @AppStorage(UserDefaults.Keys.UserView.timeFormat) var preferredTimeFormat: TimeFormat = .twelveHour
+    @AppStorage(UserDefaults.Keys.UserView.appTheme) var appTheme: String = "Light"
+    @AppStorage(UserDefaults.Keys.UserView.appIcon) var appIcon: String = "Default"
+    @AppStorage(UserDefaults.Keys.UserView.appLanguage) var appLanguage: String = "English (US)"
+    
     var body: some View {
         NavigationStack {
             List {
                 Section {
-                    Text("My Info #1")
-                    Text("My Info #2")
-                    Text("My Info #3")
-                    Text("My Info #4")
+                    DescriptionTapToActionSheetRowView(title: "Name", description: "Ness P.", options: [])
+                    
+                    DescriptionTapToActionSheetRowView(
+                        title: "Skin Type",
+                        description: userSkinType.description,
+                        options: SkinType.allCases.map { skinType in
+                            return (title: skinType.description, action: { userSkinType = skinType })
+                        }
+                    )
                 }
                 Section("Settings") {
-                    Text("Setting #1")
+                    DescriptionTapToActionSheetRowView(
+                        title: "Units",
+                        description: preferredTemperatureUnit == .both ? "°C | °F" : preferredTemperatureUnit.description,
+                        options: TemperatureUnit.allCases.map { temperatureUnit in
+                            return (title: temperatureUnit.description, action: { preferredTemperatureUnit = temperatureUnit })
+                        }
+                    )
+                    DescriptionTapToActionSheetRowView(
+                        title: "Time Format",
+                        description: preferredTimeFormat.description,
+                        options: TimeFormat.allCases.map { timeFormat in
+                            return (title: timeFormat.description, action: { preferredTimeFormat = timeFormat })
+                        }
+                    )
+                }
+                Section("Theme") {
+                    DescriptionTapToActionSheetRowView(title: "Theme", description: appTheme, options: [])
+                    DescriptionTapToActionSheetRowView(title: "App Icon", description: appIcon, options: [])
+                    DescriptionTapToActionSheetRowView(title: "Language", description: appLanguage, options: [])
+                }
+                Section("About") {
+                    DescriptionTapToActionSheetRowView(title: "Info", description: "", options: [])
+                    DescriptionTapToActionSheetRowView(title: "About Dewy", description: "", options: [])
                 }
             }
             .navigationTitle("About Me")
