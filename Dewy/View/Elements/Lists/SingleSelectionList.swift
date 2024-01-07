f6f641ddef0e3b7065f58711e988ce087a9bc211
Alexander Latimer <zanderlatimer@gmail.com>
2024-01-07 10:17:38 -0800

    Added a SingleSelectionList view that allows a single row to be selected, displaying a checkmark.

---
+++ Dewy/View/Elements/Lists/SingleSelectionList.swift
@@ -0,0 +1,51 @@
+//
+//  SingleSelectionList.swift
+//  Dewy
+//
+//  Created by Zander Latimer on 9/26/23.
+//
+
+import SwiftUI
+
+struct SingleSelectionList<ListItem: UniqueDescribable>: View {
+    
+    @Binding private var selectedListItem: ListItem?
+    
+    private let listItems: [ListItem]
+    private let selectionChangedAction: ((ListItem?) -> ())?
+    
+    var body: some View {
+        List {
+            ForEach(listItems, id: \.self) { listItem in
+                CheckmarkListRow<ListItem>(listItem: listItem, selectedListItem: $selectedListItem)
+            }
+        }
+        .onChange(of: selectedListItem) { newValue in
+            selectionChangedAction?(newValue)
+        }
+    }
+    
+    init(listItems: [ListItem], selectedListItem: Binding<ListItem?>, selectionChangedAction: ((ListItem?) -> ())? = nil) {
+            
+        self.listItems = listItems
+        self._selectedListItem = selectedListItem
+        self.selectionChangedAction = selectionChangedAction
+    }
+}
+
+struct ThemeSelectView_Previews: PreviewProvider {
+    static var previews: some View {
+        let listItemA = AppTheme.system
+        let listItemB = AppTheme.light
+        let listItemC = AppTheme.dark
+        
+        SingleSelectionList(
+            listItems: [
+                listItemA,
+                listItemB,
+                listItemC
+            ],
+            selectedListItem: .constant(listItemA)
+        )
+    }
+}
