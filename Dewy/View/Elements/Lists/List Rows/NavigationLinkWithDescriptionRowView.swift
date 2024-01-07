ca0e99e0ec71c694d000bc62195920b2f5c75cf1
Alexander Latimer <zanderlatimer@gmail.com>
2024-01-07 10:18:51 -0800

    Added a NavigationLinkWithDescriptionRowView that functions as a List row with disclosure indicator to navigate to a different view.

---
+++ Dewy/View/Elements/Lists/List Rows/NavigationLinkWithDescriptionRowView.swift
@@ -0,0 +1,45 @@
+//
+//  NavigationLinkWithDescriptionRowView.swift
+//  Dewy
+//
+//  Created by Zander Latimer on 9/26/23.
+//
+
+import SwiftUI
+
+struct NavigationLinkWithDescriptionRowView<LinkContent: View>: View {
+    
+    private let title: String
+    private let description: String
+    
+    @ViewBuilder private let linkDestination: () -> LinkContent
+    
+    var body: some View {
+        NavigationLink {
+            linkDestination()
+        } label: {
+            HStack {
+                Text(title)
+                Spacer()
+                Text(description)
+                    .foregroundColor(.secondary)
+            }
+        }
+    }
+    
+    init(title: String, description: String, @ViewBuilder linkDestination: @escaping () -> LinkContent) {
+        self.title = title
+        self.description = description
+        self.linkDestination = linkDestination
+    }
+}
+
+struct NavigationLinkWithDescriptionRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationLinkWithDescriptionRowView(
+            title: "Title",
+            description: "Description",
+            linkDestination: { Text("Link Destination") }
+        )
+    }
+}
