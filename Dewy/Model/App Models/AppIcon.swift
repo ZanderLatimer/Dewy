00de6eca05d0711c42f190246a92ff9a9e9bb4d7
Alexander Latimer <zanderlatimer@gmail.com>
2024-01-08 14:20:19 -0800

    Moved AppIcon into its own file.

--- Dewy/Model/App Models/AppIcon.swift
+++ Dewy/Model/App Models/AppIcon.swift
@@ -5,4 +5,56 @@
 //  Created by Zander Latimer on 7/1/24.
 //
 
-import Foundation
+import SwiftUI
+
+enum AppIcon: String, CaseIterable, UniqueDisplayable {
+    
+    typealias Content = AnyView
+    
+    case original = "AppIcon"
+    case purple = "AppIconPurple"
+    case green = "AppIconGreen"
+    case orange = "AppIconOrange"
+    case black = "AppIconBlack"
+    
+    var id: String {
+        return rawValue
+    }
+    
+    var iconName: String? {
+        switch self {
+        case .original: return nil
+        default: return id
+        }
+    }
+    
+    var preview: Image? {
+        Image(id + "-Preview")
+    }
+
+    var description: String {
+        switch self {
+        case .original: return "Original"
+        case .purple: return "Purple"
+        case .green: return "Green"
+        case .orange: return "Orange"
+        case .black: return "Black"
+        }
+    }
+    
+    var descriptionView: Content {
+        AnyView(
+            HStack {
+                if let preview = preview {
+                    preview
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 60, height: 60)
+                        .cornerRadius(12)
+                }
+                
+                Text(description)
+            }
+        )
+    }
+}
